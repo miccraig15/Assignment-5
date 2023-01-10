34ddf94d5f6543c8b0dd4b95946ec2734914fe1f
miccraig15 <miccraig15@gmail.com>
2023-01-10 17:10:11 -0600

    Added preventDefault validation

--- script.js
+++ script.js
@@ -12,22 +12,33 @@
 <img src="${}">
 */
 
-//TODO 1: set up an window load handler
+
 
 window.addEventListener("load", function() {
-      submit.addEventListener("click", function (event) {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
          let pilotName = document.getElementById("pilotName");
+         let copilotName = document.getElementById("copilotName");
+         let fuelLevel = document.getElementById("fuelLevel");
+         let cargoMass = document.getElementById("cargoMass");
+            if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+               event.preventDefault();
+               window.alert("All fields are required!")
+            } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+               event.preventDefault();
+               window.alert("Invalid Entry. Fuel and Cargo must be a number!")
+            }
    });
 
 });
 
-   // TODO 2: set up a submit handler for the form
+//TODO 1: set up an window load handler ***complete***
 
+   // TODO 2: set up a submit handler for the form ***complete***
 
+      //TODO 3: cancel submission using event preventDefault() ***complete***
 
-      //TODO 3: cancel submission using event preventDefault()
-
-      //TODO 4: validate that all inputs have data and data type is correct
+      //TODO 4: validate that all inputs have data and data type is correct ***complete***
 
       //TODO 5: check fuel level and cargo mass, and report launch status
 
