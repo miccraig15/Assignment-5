5f4785d60d3591a9ed7cb762907a75d8a8325593
miccraig15 <miccraig15@gmail.com>
2023-01-10 15:39:19 -0600

    initial commit

--- script.js
+++ script.js
@@ -11,3 +11,30 @@
 </ul>
 <img src="${}">
 */
+
+//TODO 1: set up an window load handler
+
+window.addEventListener("load", function() {
+      submit.addEventListener("click", function (event) {
+         let pilotName = document.getElementById("pilotName");
+   });
+
+});
+
+   // TODO 2: set up a submit handler for the form
+
+
+
+      //TODO 3: cancel submission using event preventDefault()
+
+      //TODO 4: validate that all inputs have data and data type is correct
+
+      //TODO 5: check fuel level and cargo mass, and report launch status
+
+      //TODO 6: make the list visible
+
+   //TODO 7: fetch planet data
+
+      //TODO 8: randomly choose one of the planets
+
+      //TODO 9: display info about the chosen planet
